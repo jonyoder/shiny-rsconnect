f8249fe05e8b32f7dec1db9dc2ff69f4aab5ad2d
jonyoder <jonyoder@gmail.com>
2016-08-09 12:00:22 -0400

    More complicated text

--- app.R
+++ app.R
@@ -6,6 +6,13 @@ library(shinyAce)
 # Default text for editor
 defaultMarkdown <- '
 ### Sample R Markdown
+This is some markdown text. It may also have embedded R code
+which will be executed.
+```{r}
+2*3
+rnorm(5)
+```
+It can even include graphical elements.
 ```{r}
 hist(rnorm(100))
 ```
